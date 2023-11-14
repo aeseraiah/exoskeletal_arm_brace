bb0448953eb1c67e347acc66d99bcd18b26ac8d5
aeseraia <aeseraia@iu.edu>
2023-11-14 08:32:25 -0500

    adding emg collection testing file

---
+++ local_testing/generate_timestamps.py
@@ -0,0 +1,15 @@
+import datetime
+import random
+
+def generate_emg_data(file_name, downsample_rate):
+    with open(file_name, 'w') as file:
+        for _ in range(downsample_rate):
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            emg_str_value = 'digital value = 350 analog value = 1.80'
+            space_sep_strs = emg_str_value.split()
+            digital_emg_value = space_sep_strs[3]
+            analog_emg_value = float(space_sep_strs[7])
+            line = f"{current_time}\t{digital_emg_value}\t{analog_emg_value}\t{emg_str_value}\t\n"
+            file.write(line)
+# Example usage
+generate_emg_data("emg_data.txt", 10)
