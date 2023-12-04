395c422dda28531e354b7a1963a01f9f6dfc6bda
Hudson, Isaac Shawn <isahudso@iu.edu>
2023-12-04 14:15:31 -0500

    Updated GravSensor and get_data fiiles

---
+++ local_testing/collection_data/arduino/get_data.ino
@@ -0,0 +1,145 @@
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include "EMGFilters.h"
+using namespace std;
+//#include "algorithm.h"
+#define TIMING_DEBUG 1
+//File file;
+
+#define SensorInputPin A0 // input pin number
+
+EMGFilters myFilter;
+// discrete filters must works with fixed sample frequence
+// our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
+// other sampleRate inputs will bypass all the EMG_FILTER
+int sampleRate = SAMPLE_FREQ_1000HZ;
+// For countries where power transmission is at 50 Hz
+// For countries where power transmission is at 60 Hz, need to change to
+// "NOTCH_FREQ_60HZ"
+// our emg filter only support 50Hz and 60Hz input
+// other inputs will bypass all the EMG_FILTER
+int humFreq = NOTCH_FREQ_60HZ;
+
+// Calibration:
+// put on the sensors, and release your muscles;
+// wait a few seconds, and select the max value as the threshold;
+// any value under threshold will be set to zero
+//static int Threshold = 0;
+
+unsigned long timeStamp;
+unsigned long timeBudget;
+
+void setup() {
+    /* add setup code here */
+    myFilter.init(sampleRate, humFreq, true, true, true);
+
+    // open serial
+    Serial.begin(115200);
+
+    // setup for time cost measure
+    // using micros()
+    timeBudget = 1e6 / sampleRate;
+    //myservo.attach(9);
+    // micros will overflow and auto return to zero every 70 minutes
+}
+
+int calc_Thresh(){
+  //loop the function until good enough data has been collected to continue
+    while(1){
+      //sample at 20/second
+      //instantiate a list to hold 30 seconds of thresholding data
+
+      //most likely problem is improper initialization of list
+      //unsigned long data[100];
+      
+      //start a timer for 30 seconds
+      Serial.println("");
+      Serial.println("Relax your arm with the sensor in place for 5 seconds."); 
+      Serial.println("Try to keep bicep activity at a minimum.");
+      Serial.println("The values printed to the screen should be as low as possible.");
+      Serial.println("A message will appear when 5 seconds has passed.");
+      long flushTime = millis();
+      unsigned long Value, DataAfterFilter, envlope;
+      //delay data collection for 4 seconds to allow for reading of the message, flush 4 seconds of values
+      while (millis() - flushTime < 4000){
+        Value = analogRead(SensorInputPin); 
+        DataAfterFilter = myFilter.update(Value);
+        envlope = sq(DataAfterFilter);
+      }
+      unsigned long Threshold = 0;
+      long startTime = millis();
+      //loop for 5 seconds
+      while (millis() - startTime < 5000){
+        Value = analogRead(SensorInputPin); 
+        DataAfterFilter = myFilter.update(Value);
+        envlope = sq(DataAfterFilter);
+        Serial.println(envlope);
+        if (envlope > Threshold) {
+              Threshold = envlope;
+          }
+      }
+      Serial.println("");
+      Serial.println("5 seconds has passed. ");
+      //if highest value from past 5 seconds is below a certain signal quality threshold, set threshold for resting muscle activity to the highest value
+      if (Threshold <= 1000){
+        Serial.println("");
+        Serial.println("Automated Thresholding Complete.");
+        Serial.print("Calculated Threshold = ");
+        Serial.println(Threshold);
+        return(Threshold);
+      }
+      else {
+        Serial.println("");
+        Serial.print("Calculated Threshold = ");
+        Serial.println(Threshold);
+        Serial.println("Bad data, adjust the sensor. Automated Thresholding will be attempted again in 5 seconds.");
+        delay(5000);
+      }
+    }
+}
+
+void gen_data(unsigned long thresh) {
+    Serial.println("Collecting Data for the next two minutes.");
+    Serial.println("Start relaxed for 5 seconds, then flex arm for 5 seconds. Repeat for the duration.");
+    delay(2000);
+    unsigned long Value, DataAfterFilter, envlope;
+    unsigned long max = thresh;
+    long startTime = millis();
+    //loop for 2 minutes seconds
+    Serial.print("Start time = ");
+    Serial.println(startTime);
+    while (millis() - startTime < 180000){
+      //gather data from .5 seconds, if any values are above thresh (demonstrating activated muscle), the servomotor will "flex" the brace
+      //check time to see if flexion or extension should be recorded
+      if ((millis() - startTime)%5000 == 0){
+        if ((millis() - startTime)%10000 == 0){
+          Serial.println("Initiate Extension.");
+        }
+        else {
+          Serial.println("Initiate Flexion.");
+        }
+      }
+      Value = analogRead(SensorInputPin); 
+      DataAfterFilter = myFilter.update(Value);
+      envlope = sq(DataAfterFilter);
+      envlope = (envlope > thresh) ? envlope : 0;
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.println(envlope);
+      myfile << millis() << "," << envlope << "\n";
+    }
+    Serial.println("Data Collection Finished");
+    Serial.print("End time = ");
+    Serial.println(millis());
+}
+
+
+void loop() {s
+    unsigned long Threshold;
+    Threshold = calc_Thresh();
+
+    gen_data(Threshold);
+}
