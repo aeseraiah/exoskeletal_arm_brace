9ec87a09d5543df0b19462653b618946af0ca0f8
Hudson, Isaac Shawn <isahudso@iu.edu>
2024-01-25 10:17:29 -0500

    2 sensor test at 500 Hz
    Added timing features to keep a consistent sample rate for the filters to apply correctly.

--- local_testing/collection_data/arduino/2sensortest.ino
+++ local_testing/collection_data/arduino/2sensortest.ino
@@ -8,18 +8,18 @@
 #define BiSensorInputPin A0   // bicep input pin number
 #define TriSensorInputPin A1  // tricep input pin number
 
-EMGFilters myFilter;
+EMGFilters myFilter1, myFilter2;
 // discrete filters must works with fixed sample frequence
 // our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
 // other sampleRate inputs will bypass all the EMG_FILTER
-int sampleRate = SAMPLE_FREQ_1000HZ;
+int sampleRate = SAMPLE_FREQ_500HZ;
 // For countries where power transmission is at 50 Hz
 // For countries where power transmission is at 60 Hz, need to change to
 // "NOTCH_FREQ_60HZ"
 // our emg filter only support 50Hz and 60Hz input
 // other inputs will bypass all the EMG_FILTER
 int humFreq = NOTCH_FREQ_60HZ;
-
+unsigned long timeBudget;
 // Calibration:
 // put on the sensors, and release your muscles;
 // wait a few seconds, and select the max value as the threshold;
@@ -28,48 +28,72 @@ int humFreq = NOTCH_FREQ_60HZ;
 
 void setup() {
   // put your setup code here, to run once:
-  myFilter.init(sampleRate, humFreq, true, true, true);
-
+  myFilter1.init(sampleRate, humFreq, true, true, true);
+  myFilter2.init(sampleRate, humFreq, true, true, true);
+  timeBudget = 1e6 / sampleRate;
   // open serial
-  Serial.begin(2000000);
+  Serial.begin(230400);
 }
 
 unsigned long readBi() {
     unsigned long biValue, biDataAfterFilter, bienvlope;
     biValue = analogRead(BiSensorInputPin);
-    //biDataAfterFilter = myFilter.update(biValue);
-    //bienvlope = sq(biDataAfterFilter);
-    return(biValue);
+    biDataAfterFilter = myFilter1.update(biValue);
+    bienvlope = sq(biDataAfterFilter);
+    return(bienvlope);
 }
 
 unsigned long readTri() {
     unsigned long triValue, triDataAfterFilter, trienvlope;
     triValue = analogRead(TriSensorInputPin);
-    //triDataAfterFilter = myFilter.update(triValue);
-    //trienvlope = sq(triDataAfterFilter);
-    return(triValue);
+    triDataAfterFilter = myFilter2.update(triValue);
+    trienvlope = sq(triDataAfterFilter);
+    return(trienvlope);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned long start = millis();
-  unsigned int bi = 0;
-  unsigned int tri = 0;
-  unsigned long time = 0;
+  
+  int bienvlope;
+  int trienvlope;
+
   int samples = 0;
-  while(1){
-    bi = readBi();
-    tri = readTri();
-    time = millis() - start;
-    Serial.print(F("Time since beginning in milliseconds: "));
-    Serial.println(time);
-    Serial.print(F("Bicep Sensor Value = "));
-    Serial.println(bi);
-    Serial.print(F("Tricep Sensor Value = "));
-    Serial.println(tri);
+  unsigned long time, initial, start, end;
+  unsigned int bi[250];
+  unsigned int tri[250];
+  initial = micros();
+  while(samples < 250){
+    //read in and filter bicep sensor data
+    start = micros();
+    bi[samples] = readBi();
+    //delayMicroseconds(50);
+    tri[samples] = readTri();
+    //Serial.println(F("Bicep Time: Tricep Time:  Total Time:"));
+    //Serial.print(bicepTime - start);
+    //Serial.print(F("  "));
+    //Serial.print(tricepTime - bicepTime);
+    //Serial.print(F("  "));
+    //Serial.println(total - start);
+    //Serial.print(F("Time since beginning in milliseconds: "));
+    //Serial.println(time);
+    //Serial.print(F("Bicep Sensor Value = "));
+    //Serial.println(bi[samples]);
+    //Serial.print(F("Tricep Sensor Value = "));
+    //Serial.println(tri[samples]);
     samples += 1;
-    Serial.print(F("current sampling rate in Hz: "));
-    Serial.println(float(samples/(time/1000)));
+    end = micros();
+    delayMicroseconds(timeBudget - (end-start));
+    
+    //Serial.print(F("current sampling rate in Hz: "));
+    //Serial.println(float(samples/(time/1000)));
     //delay(100);
   }
+  Serial.println(timeBudget);
+  time = micros() - initial;
+  Serial.println(time);
+  
+  Serial.print(F("current sampling rate in Hz: "));
+  Serial.println(double(samples*1000000)/double(time));
+  delay(2000);
 }
